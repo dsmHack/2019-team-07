1d6591bd797ddc9aa8b14759335e1c8f3991b261
Eric Ponto <ponto.eric@principal.com>
2019-02-21 23:41:42 -0600

    basic nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,16 +3,27 @@ import Link from "gatsby-link";
 import styles from "./navigation.module.css";
 
 export default () => (
-	<nav role="navigation">
-		<ul className={styles.navigation}>
+	<nav role="navigation" className={styles.navigation}>
+		<ul className={styles.navigationList}>
 			<li className={styles.navigationItem}>
-				<Link to="/">Home</Link>
+				<Link to="/">
+					<img src="./logo.jpg" /> Welcome
+				</Link>
 			</li>
 			<li className={styles.navigationItem}>
-				<Link to="/blog/">Blog</Link>
+				<Link to="/">About Us</Link>
 			</li>
 			<li className={styles.navigationItem}>
-				<Link to="/members/">Members</Link>
+				<Link to="/">New & Events</Link>
+			</li>
+			<li className={styles.navigationItem}>
+				<Link to="/">Giving</Link>
+			</li>
+			<li className={styles.navigationItem}>
+				<Link to="/">Contact</Link>
+			</li>
+			<li className={styles.navigationItem}>
+				<button className={styles.navButton}>Sign In</button>
 			</li>
 		</ul>
 	</nav>
