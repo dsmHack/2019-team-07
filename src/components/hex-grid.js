3dbc69f3bc70ed33245b12fe62fe47ea14d7b9a0
Shane Church <shane@s-church.net>
2019-02-22 16:18:13 -0600

    Additional styling for hexagon grid

--- src/components/hex-grid.js
+++ src/components/hex-grid.js
@@ -3,8 +3,9 @@ import React from 'react';
 import styles from './hex-grid.module.css';
 
 export default ({ data }) => (
-  <ul className={styles.hexGrid}>
-    {data.map((item, index) => 
+  <div className={styles.hexContainer}>
+    <ul className={styles.hexGrid}>
+      {data.map((item, index) => (
         <li className={styles.hex} key={item.contentful_id}>
           <div className={styles.hexIn}>
             <a className={styles.hexLink} href="#">
@@ -12,6 +13,7 @@ export default ({ data }) => (
             </a>
           </div>
         </li>
-    )}
-  </ul>
+      ))}
+    </ul>
+  </div>
 );
