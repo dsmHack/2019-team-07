f6ae56bf955c1c16b172bd36b35b1435e97eb18c
Eric Ponto <ponto.eric@principal.com>
2019-02-22 11:56:38 -0600

    added icons in the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,26 @@
 import React from "react";
 import styles from "./footer.module.css";
+import { Facebook, Instagram, Twitter } from "react-feather";
 
 export default () => (
 	<div className={styles.footer}>
-		<div class="wrapper">
-			<div class="row">
+		<div className="wrapper">
+			<div className="row">
 				<div>
 					<p>Les Dames d'Escossier | Greater Des Moines Chapter</p>
 					<p>&copy; {new Date().getFullYear()}</p>
 				</div>
-				<div>Twitter, etc..</div>
+				<div className="row">
+					<a href="https://www.facebook.com/damesdsm/" aria-label="Dames DSM Facebook">
+						<Facebook />
+					</a>
+					<a href="https://www.twitter.com" aria-label="Dames DSM Facebook">
+						<Twitter />
+					</a>
+					<a href="https://www.facebook.com/damesdsm/" aria-label="Dames DSM Facebook">
+						<Instagram />
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
