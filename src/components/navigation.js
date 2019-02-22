3e1382fcdbe4f95b61efc10f6419f1c040cd8fb3
Eric Ponto <ponto.eric@principal.com>
2019-02-21 22:18:31 -0600

    added members page

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import styles from './navigation.module.css'
+import React from "react";
+import Link from "gatsby-link";
+import styles from "./navigation.module.css";
 
 export default () => (
-  <nav role="navigation">
-    <ul className={styles.navigation}>
-      <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
-      </li>
-    </ul>
-  </nav>
-)
+	<nav role="navigation">
+		<ul className={styles.navigation}>
+			<li className={styles.navigationItem}>
+				<Link to="/">Home</Link>
+			</li>
+			<li className={styles.navigationItem}>
+				<Link to="/blog/">Blog</Link>
+			</li>
+			<li className={styles.navigationItem}>
+				<Link to="/members/">Members</Link>
+			</li>
+		</ul>
+	</nav>
+);
