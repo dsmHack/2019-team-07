8b4f939aa8a75721296b5cba7e261d3e9f0dfb13
Eric Ponto <ponto.eric@principal.com>
2019-02-22 16:12:28 -0600

    updated membership page

--- src/pages/membership.js
+++ src/pages/membership.js
@@ -4,16 +4,36 @@ import Helmet from "react-helmet";
 import Layout from "../layouts";
 import { graphql } from "gatsby";
 import styles from "./membership.module.css";
+import HeroImage from "../components/hero-image";
 
 class RootIndex extends React.Component {
 	render() {
 		const siteTitle = get(this, "props.data.site.siteMetadata.title");
 		const members = get(this, "props.data.allContentfulMember.edges");
+		const membership = get(this, "props.data.allContentfulMembership.edges");
 
 		return (
 			<Layout>
 				<div>
 					<Helmet title={`Membership - ${siteTitle}`} />
+					{membership.map(({ node }) => (
+						<div>
+							<HeroImage photos={node.photos} title={node.title} />
+							<div className="lead">
+								<div
+									dangerouslySetInnerHTML={{
+										__html: node.lead.childMarkdownRemark.html
+									}}
+								/>
+							</div>
+							<h2 className="section-headline text-centered">{node.bodyTitle}</h2>
+							<div
+								dangerouslySetInnerHTML={{
+									__html: node.body.childMarkdownRemark.html
+								}}
+							/>
+						</div>
+					))}
 					<div className="wrapper">
 						<h2 className="section-headline text-centered">Meet our Members</h2>
 						<ul className={styles.memberList}>
@@ -39,6 +59,29 @@ export default RootIndex;
 
 export const pageQuery = graphql`
 	query MembershipQuery {
+		allContentfulMembership {
+			edges {
+				node {
+					title
+					lead {
+						childMarkdownRemark {
+							html
+						}
+					}
+					bodyTitle
+					body {
+						childMarkdownRemark {
+							html
+						}
+					}
+					photos {
+						sizes(maxWidth: 400, maxHeight: 300, resizingBehavior: FILL) {
+							...GatsbyContentfulSizes_withWebp
+						}
+					}
+				}
+			}
+		}
 		allContentfulMember(sort: { fields: [name], order: ASC }) {
 			edges {
 				node {
