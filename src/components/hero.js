f4355b14b544acc74d4cca5e66777125af82dba2
Shane Church <shane@s-church.net>
2019-02-22 14:07:56 -0600

    WIP Hero Images

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,18 +5,44 @@ import styles from './hero.module.css';
 
 export default ({ data }) => (
   <div className={styles.hero}>
-    <div className={styles.heroSecondarySection} />
+    <ul className={styles.heroSecondarySection + " " + styles.hexGrid}>
+      {Array.apply(0, Array(25)).map((x, i) => {
+        return (
+          <li className={styles.hex}>
+            <div className={styles.hexIn}>
+              <a className={styles.hexLink} href="#">
+                <img
+                  src="https://farm9.staticflickr.com/8461/8048823381_0fbc2d8efb.jpg"
+                  alt=""
+                />
+              </a>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
     <div className={styles.heroMainSection}>
       <Img
         className={styles.heroImage}
         alt={data.name}
         sizes={data.heroImage.sizes}
       />
-      <div className={styles.heroDetails}>
-        <h3 className={styles.heroHeadline}>{data.name}</h3>
-        <p className={styles.heroTitle}>{data.title}</p>
-      </div>
     </div>
-    <div className={styles.heroSecondarySection} />
+    <ul className={styles.heroSecondarySection + " " + styles.hexGrid}>
+      {Array.apply(0, Array(25)).map((x, i) => {
+        return (
+          <li className={styles.hex}>
+            <div className={styles.hexIn}>
+              <a className={styles.hexLink} href="#">
+                <img
+                  src="https://farm9.staticflickr.com/8461/8048823381_0fbc2d8efb.jpg"
+                  alt=""
+                />
+              </a>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
   </div>
 );
