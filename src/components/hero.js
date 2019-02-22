b9097854807aa9a1afb1903fca26ef76477c1cff
Shane Church <shane@s-church.net>
2019-02-22 12:37:55 -0600

    Hooking up hero image data

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import Img from 'gatsby-image'
+import Img from 'gatsby-image';
+import React from 'react';
 
-import styles from './hero.module.css'
+import styles from './hero.module.css';
 
 export default ({ data }) => (
   <div className={styles.hero}>
-    <Img className={styles.heroImage} alt={data.name} sizes={data.heroImage.sizes} />
-    <div className={styles.heroDetails}>
-      <h3 className={styles.heroHeadline}>{data.name}</h3>
-      <p className={styles.heroTitle}>{data.title}</p>
-      <p>{data.shortBio.shortBio}</p>
+    <div className={styles.heroSecondarySection} />
+    <div className={styles.heroMainSection}>
+      <Img
+        className={styles.heroImage}
+        alt={data.name}
+        sizes={data.heroImage.sizes}
+      />
+      <div className={styles.heroDetails}>
+        <h3 className={styles.heroHeadline}>{data.name}</h3>
+        <p className={styles.heroTitle}>{data.title}</p>
+      </div>
     </div>
+    <div className={styles.heroSecondarySection} />
   </div>
-)
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,65 +1,94 @@
-import { graphql } from "gatsby";
-import get from "lodash/get";
-import React from "react";
-import Helmet from "react-helmet";
-import Mission from "../components/mission";
-import Feature from "../components/feature";
-import Layout from "../layouts";
+import { graphql } from 'gatsby';
+import get from 'lodash/get';
+import React from 'react';
+import Helmet from 'react-helmet';
+
+import Feature from '../components/feature';
+import Hero from '../components/hero';
+import Mission from '../components/mission';
+import Layout from '../layouts';
 
 class RootIndex extends React.Component {
-	render() {
-		const siteTitle = get(this, "props.data.site.siteMetadata.title");
-		const features = get(this, "props.data.allContentfulFeature.edges");
-		const mission = get(this, "props.data.allContentfulMissionStatement.edges");
+  render() {
+    const siteTitle = get(this, "props.data.site.siteMetadata.title");
+    const features = get(this, "props.data.allContentfulFeature.edges");
+    const mission = get(this, "props.data.allContentfulMissionStatement.edges");
+	const page = get(this, "props.data.allContentfulBasicPage.edges");
 
-		return (
-			<Layout>
-				<div>
-					<Helmet title={siteTitle} />
-					<Mission mission={mission[0].node} />
-					<div className="wrapper">
-						<div className="row">{features.map(({ node }) => <Feature feature={node} />)}</div>
-					</div>
-				</div>
-			</Layout>
-		);
-	}
+    return (
+      <Layout>
+        <div>
+          <Helmet title={siteTitle} />
+          <Hero data={page[0].node} />
+          <Mission mission={mission[0].node} />
+          <div className="wrapper">
+            <div className="row">
+              {features.map(({ node }) => (
+                <Feature feature={node} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
 }
 
 export default RootIndex;
 
 export const pageQuery = graphql`
-	query HomeQuery {
-		allContentfulFeature {
-			edges {
-				node {
-					title
-					image {
-						sizes(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
-							...GatsbyContentfulSizes_withWebp
-						}
-					}
-					description {
-						childMarkdownRemark {
-							html
-						}
-					}
-					callToActionText
-					callToActionUrl
-				}
-			}
-		}
-		allContentfulMissionStatement {
-			edges {
-				node {
-					title
-					body {
-						childMarkdownRemark {
-							html
-						}
-					}
-				}
-			}
-		}
-	}
+  query HomeQuery {
+    allContentfulFeature {
+      edges {
+        node {
+          title
+          image {
+            sizes(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
+              ...GatsbyContentfulSizes_withWebp
+            }
+          }
+          description {
+            childMarkdownRemark {
+              html
+            }
+          }
+          callToActionText
+          callToActionUrl
+        }
+      }
+    }
+    allContentfulMissionStatement {
+      edges {
+        node {
+          title
+          body {
+            childMarkdownRemark {
+              html
+            }
+          }
+        }
+      }
+    }
+    allContentfulBasicPage(
+      filter: { contentful_id: { eq: "7xXpIF01hW7xFEyGqa5SI2" } }
+    ) {
+      edges {
+        node {
+          title
+
+          heroImage {
+            title
+            fluid(maxWidth: 1200) {
+              sizes
+              src
+              srcSet
+            }
+            sizes(resizingBehavior: SCALE) {
+              ...GatsbyContentfulSizes_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
 `;
