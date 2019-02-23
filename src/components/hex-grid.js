5517e197856ef1cd3f454aa613b2f7991557d817
Shane Church <shane@s-church.net>
2019-02-22 19:45:39 -0600

    Mechanics for flipping cards

--- src/components/hex-grid.js
+++ src/components/hex-grid.js
@@ -6,10 +6,16 @@ export default ({ data }) => (
   <div className={styles.hexContainer}>
     <ul className={styles.hexGrid}>
       {data.map((item, index) => (
-        <li className={styles.hex} key={item.contentful_id}>
-          <div className={styles.hexIn}>
-            <div className={styles.hexLink}>
-              <img alt={item.title} src={item.fixed.src} />
+        <li className={styles.flipContainer} key={item.contentful_id}>
+          <div className={styles.hex}>
+            <div className={styles.hexIn + " " + styles.hexFront}>
+              <div className={styles.hexLink}>
+                <img alt={item.title} src={item.fixed.src} />
+              </div>
+            </div>
+            <div className={styles.hexIn + " " + styles.hexBack}>
+              <div className={styles.hexLink}>
+              </div>
             </div>
           </div>
         </li>
