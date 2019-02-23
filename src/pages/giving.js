ed90a2301be111aacf04c9b8b5457027fbfab374
Eric Ponto <ponto.eric@principal.com>
2019-02-22 18:46:08 -0600

    grants and scholarship first try

--- src/pages/giving.js
+++ src/pages/giving.js
@@ -3,28 +3,35 @@ import React from "react";
 import Helmet from "react-helmet";
 import Layout from "../layouts";
 import { graphql } from "gatsby";
+import HeroImage from "../components/hero-image";
 
 class RootIndex extends React.Component {
 	render() {
 		const siteTitle = get(this, "props.data.site.siteMetadata.title");
-		const giving = get(this, "props.data.allContentfulGiving.edges");
+		const giving = get(this, "props.data.allContentfulGrantsScholarships.edges");
 
 		return (
 			<Layout>
-				<div style={{ background: "#fff" }}>
-					<Helmet title={`Giving - ${siteTitle}`} />
-					<div className="wrapper">
-						<h2 className="section-headline">Giving</h2>
-						<ul className="Giving-list">
-							{giving.map(({ node }) => {
-								return (
-									<li key={node.id}>
-										{node.title}
-									</li>
-								);
-							})}
-						</ul>
-					</div>
+				<div>
+					<Helmet title={`Grants & Scholarship - ${siteTitle}`} />
+					{giving.map(({ node }) => (
+						<div>
+							<HeroImage photos={node.photos} title={node.title} />
+							<div className="lead">
+								<div
+									dangerouslySetInnerHTML={{
+										__html: node.lead.childMarkdownRemark.html
+									}}
+								/>
+							</div>
+							<h2 className="section-headline text-centered">{node.bodyTitle}</h2>
+							<div
+								dangerouslySetInnerHTML={{
+									__html: node.body.childMarkdownRemark.html
+								}}
+							/>
+						</div>
+					))}
 				</div>
 			</Layout>
 		);
@@ -35,24 +42,26 @@ export default RootIndex;
 
 export const pageQuery = graphql`
 	query GivingQuery {
-		allContentfulGiving(sort: { fields: [title], order: ASC }) {
+		allContentfulGrantsScholarships {
 			edges {
 				node {
-                    title
-                    scholarship
-                    scholarshipPhoto{
-						sizes(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
+					title
+					lead {
+						childMarkdownRemark {
+							html
+						}
+					}
+					body {
+						childMarkdownRemark {
+							html
+						}
+					}
+					bodyTitle
+					photos {
+						sizes(maxWidth: 400, maxHeight: 300, resizingBehavior: FILL) {
 							...GatsbyContentfulSizes_withWebp
 						}
 					}
-                    grant
-                    grantPhoto{
-						sizes(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
-							...GatsbyContentfulSizes_withWebp
-						}
-					}
-                    callToActionText
-                    callToActionUrl
 				}
 			}
 		}
