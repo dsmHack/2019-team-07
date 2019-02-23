af6e4d6e2bb771504ebeb7a1c62eccad01fd8c3e
Eric Ponto <ponto.eric@principal.com>
2019-02-23 09:48:43 -0600

    social media icons from contentful

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,26 +1,55 @@
+import { graphql, StaticQuery } from "gatsby";
 import React from "react";
-import styles from "./footer.module.css";
 import { Facebook, Instagram, Twitter } from "react-feather";
+import styles from "./footer.module.css";
 
 export default () => (
-	<div className={styles.footer}>
-		<div className="container">
-			<div className="row row-center">
-				<div>
-					&copy; {new Date().getFullYear()} - Les Dames d'Escossier International · Greater Des Moines Chapter
+	<StaticQuery
+		query={graphql`
+			query FooterQuery {
+				allContentfulSocialMedia(filter: { title: { eq: "Social Media Links" } }) {
+					edges {
+						node {
+							facebook
+							twitter
+							instagram
+						}
+					}
+				}
+			}
+		`}
+		render={(data) => {
+			const node = data.allContentfulSocialMedia.edges[0].node;
+
+			return (
+				<div className={styles.footer}>
+					<div className="container">
+						<div className="row row-center">
+							<div>
+								&copy; {new Date().getFullYear()} - Les Dames d'Escossier International · Greater Des
+								Moines Chapter
+							</div>
+							<div className="row">
+								{node.facebook && (
+									<a href={node.facebook} aria-label="Dames DSM Facebook">
+										<Facebook />
+									</a>
+								)}
+								{node.twitter && (
+									<a href={node.twitter} aria-label="Dames DSM Facebook">
+										<Twitter />
+									</a>
+								)}
+								{node.instagram && (
+									<a href={node.instagram} aria-label="Dames DSM Facebook">
+										<Instagram />
+									</a>
+								)}
+							</div>
+						</div>
+					</div>
 				</div>
-				<div className="row">
-					<a href="https://www.facebook.com/damesdsm/" aria-label="Dames DSM Facebook">
-						<Facebook />
-					</a>
-					<a href="https://www.twitter.com" aria-label="Dames DSM Facebook">
-						<Twitter />
-					</a>
-					<a href="https://www.facebook.com/damesdsm/" aria-label="Dames DSM Facebook">
-						<Instagram />
-					</a>
-				</div>
-			</div>
-		</div>
-	</div>
+			);
+		}}
+	/>
 );
