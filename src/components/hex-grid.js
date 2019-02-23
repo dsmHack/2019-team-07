98a0543cc3335e46e0496a462c1ff1e07521ad16
Shane Church <shane@s-church.net>
2019-02-22 21:50:07 -0600

    WIP rotating tiles

--- src/components/hex-grid.js
+++ src/components/hex-grid.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
 import styles from './hex-grid.module.css';
+import HexItem from './hex-item';
 
-export default ({ data }) => (
-  <div className={styles.hexContainer}>
-    <ul className={styles.hexGrid}>
-      {data.map((item, index) => (
-        <li className={styles.flipContainer} key={item.contentful_id}>
-          <div className={styles.hex}>
-            <div className={styles.hexIn + " " + styles.hexFront}>
-              <div className={styles.hexLink}>
-                <img alt={item.title} src={item.fixed.src} />
-              </div>
-            </div>
-            <div className={styles.hexIn + " " + styles.hexBack}>
-              <div className={styles.hexLink}>
-              </div>
-            </div>
-          </div>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+class HexGrid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: props.data };
+  }
+
+  render() {
+    return (
+      <div className={styles.hexContainer}>
+        <ul className={styles.hexGrid}>
+          {this.state.data.map((item, index) => (
+            <HexItem item={item} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default HexGrid;
